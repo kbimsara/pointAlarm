2c67fbc12847bfa29a37eab201f293afd755b832
Kavindu Bimsara <kavidubim2gamaethige@gmail.com>
2025-10-23 20:36:09 +0530

    Create myHome.dart

---
+++ lib/myHome.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyHomePage());
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Home Page'),
+      ),
+      body: Center(
+        child: Text('Welcome to My Home Page!'),
+      ),
+    );
+  }
+}
