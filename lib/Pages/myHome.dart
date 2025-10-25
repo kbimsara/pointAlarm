c6e7b066f39801b6ea26ca50dfb2d6fc9a704b27
Kavindu Bimsara <kavidubim2gamaethige@gmail.com>
2025-10-25 16:51:07 +0530

    Update myHome.dart

--- lib/Pages/myHome.dart
+++ lib/Pages/myHome.dart
@@ -8,13 +8,30 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Home Page'),
-        centerTitle: true,
-      ),
+
+      backgroundColor: Color(0xff1E1E1E),
+      appBar: appBar(),
       body: Center(
-        child: Text('Welcome to My Home Page!'),
+        child: Text('Welcome to My Home Page!',
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              color: Color(0xffEEEEEE),
+            )),
+        ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      backgroundColor: Color(0xff1E1E1E),
+      title: Text('Point Alarm'),
+      titleTextStyle: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+        color: const Color(0xffEEEEEE),
       ),
+      centerTitle: true,
     );
   }
 }
