b9ef5659ec94a4882778f8089d5ecd653312926a
Kavindu Bimsara <kavidubim2gamaethige@gmail.com>
2025-10-31 11:22:08 +0530

    Create locationService.dart

---
+++ lib/services/locationService.dart
@@ -0,0 +1,32 @@
+import 'package:geolocator/geolocator.dart';
+
+class Locationservice {
+  /// Returns the current position or throws an exception on failure.
+  Future<Position> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    final Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    // Return the found position to the caller
+    return position;
+  }
+
+}
